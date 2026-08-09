1905494d00e23fc1ea393ee934d3b1fe9f13b4e6
jonathan-borges <jonathantborges@gmail.com>
2026-08-09 02:59:58 -0300

    fix: create admin comman3

--- usuarios/create_admin.py
+++ usuarios/create_admin.py
@@ -5,7 +5,7 @@ from usuarios.models import Usuario
 
 
 class Command(BaseCommand):
-    help = "Cria o usuário administrador usando variáveis de ambiente"
+    help = "Cria o administrador inicial"
 
     def handle(self, *args, **options):
         email = os.getenv("DJANGO_SUPERUSER_EMAIL")
@@ -13,35 +13,38 @@ class Command(BaseCommand):
 
         if not email or not password:
             self.stdout.write(
-                self.style.WARNING(
-                    "DJANGO_SUPERUSER_EMAIL ou DJANGO_SUPERUSER_PASSWORD não configurados."
+                self.style.ERROR(
+                    "As variáveis DJANGO_SUPERUSER_EMAIL e "
+                    "DJANGO_SUPERUSER_PASSWORD precisam estar configuradas."
                 )
             )
             return
 
-        usuario, criado = Usuario.objects.get_or_create(
-            email=email,
-            defaults={
-                "nome": "Administrador",
-                "tipo_usuario": Usuario.TipoUsuario.ADMINISTRADOR,
-                "ativo": True,
-                "is_staff": True,
-                "is_superuser": True,
-            },
-        )
+        usuario = Usuario.objects.filter(email=email).first()
 
-        if criado:
-            usuario.set_password(password)
-            usuario.save()
-
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Administrador {email} criado com sucesso."
-                )
-            )
-        else:
+        if usuario:
             self.stdout.write(
                 self.style.WARNING(
-                    f"O usuário {email} já existe."
+                    f"O usuário {email} já existe. Nenhum usuário foi criado."
                 )
             )
+            return
+
+        usuario = Usuario(
+            email=email,
+            nome="Administrador",
+            tipo_usuario="ADMINISTRADOR",
+            ativo=True,
+            is_staff=True,
+            is_superuser=True,
+        )
+
+        usuario.set_password(password)
+        usuario.save()
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Administrador {email} criado com sucesso!"
+            )
+        )
+

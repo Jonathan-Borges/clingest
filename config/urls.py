39049557e8c02dce535b3e7b49250105d7f2bdb5
jonathan-borges <jonathantborges@gmail.com>
2026-08-09 02:27:24 -0300

    redirectcorrigido

--- config/urls.py
+++ config/urls.py
@@ -1,56 +1,23 @@
 from django.contrib import admin
-from django.urls import include
-from django.urls import path
+from django.urls import include, path
 from django.shortcuts import redirect
 
-
 urlpatterns = [
+    path("admin/", admin.site.urls),
 
-    path(
-        "admin/",
-        admin.site.urls,
-    ),
+    path("dashboard/", include("dashboard.urls")),
 
+    path("pacientes/", include("pacientes.urls")),
 
-    path(
-        "dashboard/",
-        include("dashboard.urls"),
-    ),
+    path("profissionais/", include("profissionais.urls")),
 
+    path("servicos/", include("servicos.urls")),
 
-    path(
-        "pacientes/",
-        include("pacientes.urls"),
-    ),
+    path("agendamentos/", include("agendamentos.urls")),
 
+    path("accounts/", include("django.contrib.auth.urls")),
 
-    path(
-        "profissionais/",
-        include("profissionais.urls"),
-    ),
+    path("financeiro/", include("financeiro.urls")),
 
-
-    path(
-        "servicos/",
-        include("servicos.urls"),
-    ),
-
-
-    path(
-        "agendamentos/",
-        include("agendamentos.urls"),
-    ),
-
-
-    path(
-        "accounts/",
-        include("django.contrib.auth.urls"),
-    ),
-    
-    path(
-    "financeiro/",
-    include("financeiro.urls"),
-),
-
-    path("", lambda request: redirect("accounts")),
+    path("", lambda request: redirect("login")),
 ]
